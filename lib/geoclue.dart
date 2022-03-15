27c5c7ff9883e9350c51ee3e2715eb21b7c1e90f
J-P Nurmi <jp.nurmi@canonical.com>
2022-03-15 22:25:45 +0100

    Get started

--- lib/geoclue.dart
+++ lib/geoclue.dart
@@ -1,8 +1,2 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
+/// GeoClue: The Geolocation Service
 library geoclue;
-
-export 'src/geoclue_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
