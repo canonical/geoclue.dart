0b748c86bfb0b525d18247dcdb1bac5d77effce1
J-P Nurmi <jp.nurmi@canonical.com>
2022-03-15 22:25:27 +0100

    Import Dart package template

---
+++ example/geoclue_example.dart
@@ -0,0 +1,6 @@
+import 'package:geoclue/geoclue.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/geoclue.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library geoclue;
+
+export 'src/geoclue_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
